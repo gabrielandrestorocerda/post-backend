ed9abbc0fcae9198dad9fab9ee172bdd407c10d4
Gabriel Andres Toro Cerda <gabriel.toro@usach.cl>
2023-08-14 01:23:48 -0400

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Post.create([
+    { post: 'Post 1', description: 'Descripcion post 1 desde Ruby' },
+    { post: 'Post 2', description: 'Descripcion post 2 desde Ruby' },
+    { post: 'Post 3', description: 'Descripcion post 3 desde Ruby' },
+    { post: 'Post 4', description: 'Descripcion post 4 desde Ruby' },
+    { post: 'Post 5', description: 'Descripcion post 5 desde Ruby' },
+])
